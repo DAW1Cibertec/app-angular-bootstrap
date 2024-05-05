e8ff05969abedb411c702a524f79e13463c9fcf2
DAW1Cibertec <daw1_cibertec@hotmail.com>
2024-05-04 22:11:18 -0500

    ejercicios angular

--- src/app/form-imc/form-imc.component.ts
+++ src/app/form-imc/form-imc.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-imc',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-imc.component.html',
   styleUrl: './form-imc.component.scss'
 })
 export class FormImcComponent {
 
+  imc = {
+    nombre: "",
+    peso: null,
+    talla: null,
+    valor: 0,
+    diagnostico: ""
+  };
+  mostrarResultado = false;
+  pacientes: Paciente = [];
+
+  calcularImc(){
+    this.imc.valor = Number(this.imc.peso) / (Number(this.imc.talla) * Number(this.imc.talla));
+    this.imc.diagnostico = this.diagnosticoImc(this.imc.valor);
+    this.pacientes.push({
+      nombre: this.imc.nombre, 
+      valorimc: this.imc.valor,
+      diagnostico: this.imc.diagnostico
+    });
+    this.mostrarResultado = true;
+  }
+
+  diagnosticoImc(valor: number):string{
+    let resultado = "";
+    if(valor <= 18){
+      resultado = "Bajo de peso";
+    }else if(valor <= 25){
+      resultado = "Peso normal";
+    }else if(valor <= 30){
+      resultado = "Con Sobrepeso";
+    }else if(valor <= 35){
+      resultado = "Obesidad leve";
+    }else if(valor <= 39){
+      resultado = "Obesidad media";
+    }else{
+      resultado = "Obesidad mórbida";
+    }
+    return resultado;
+  }
 }
+type Paciente = Array<{nombre: string, valorimc: number, diagnostico: string}>
--- src/app/form-suma/form-suma.component.ts
+++ src/app/form-suma/form-suma.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-suma',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-suma.component.html',
   styleUrl: './form-suma.component.scss'
 })
 export class FormSumaComponent {
+
+  suma = {
+    numero1: 0,
+    numero2: 0,
+    resultado: 0
+  }
+  mostrarResultado = false;
+
+  calcularSuma(){
+    this.suma.resultado = Number(this.suma.numero1) + Number(this.suma.numero2);    
+    this.mostrarResultado = true;
+  }
 
 }
