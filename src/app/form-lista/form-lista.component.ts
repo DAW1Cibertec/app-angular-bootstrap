4bffeafb5f82ece8fda8a3b69c80fe467b4f80c3
DAW1Cibertec <daw1_cibertec@hotmail.com>
2024-05-05 09:31:48 -0500

    uso de directivas

--- src/app/form-lista/form-lista.component.ts
+++ src/app/form-lista/form-lista.component.ts
@@ -9,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class FormListaComponent {
 
+  alumnos = [
+    {nombre: "Joan", apellido: "Osorio", nota: 18},
+    {nombre: "José", apellido: "Lagos", nota: 5},
+    {nombre: "Juan", apellido: "Mazuelos", nota: 10},
+    {nombre: "Luis", apellido: "Ayala", nota: 12},
+    {nombre: "Luis", apellido: "Rojas", nota: 20},
+    {nombre: "Nelson", apellido: "Laura", nota: 17},
+    {nombre: "Pedro", apellido: "Mejia", nota: 6},
+    {nombre: "Abel", apellido: "Perez", nota: 11},
+  ];
+
 }
